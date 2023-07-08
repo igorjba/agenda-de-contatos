5369a99837fd54cc9b269677474b199094a40802
Igor <igorjbahia@gmai.com>
2023-07-08 08:49:25 -0300

    :sparkles: feat: Foi organizado o main e crado o header.

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,20 @@
+import './styles.css'
+
+function Table() {
+    return (
+        <div className='container-table'>
+            <div className='table-header'>
+                <strong>Nome</strong>
+                <strong>E-mail</strong>
+                <strong>Telefone</strong>
+                <strong></strong>
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Table
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,20 @@
 import Header from '../../components/Header';
 import './styles.css';
+import Table from '../../components/Table';
 
 function Main() {
   return (
     <div container-main>
       <Header />
       <main>
-        <h1>Main</h1>
+
+        <button
+          className='btn-green'>
+          Adicionar
+        </button>
+
+        <Table />
+
       </main>
     </div>
   )
